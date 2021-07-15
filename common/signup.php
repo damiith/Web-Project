b9c03f0179f1b1077c10de18ef57d506a7a402a7
Damith Darshana <63197821+damiith@users.noreply.github.com>
2021-07-16 01:36:29 +0530

    Update signup.php

--- common/signup.php
+++ common/signup.php
@@ -60,7 +60,7 @@
             <a href="#">Pastpapers</a>
             <a href="../common/AboutUs.html">About Us</a>
             <a href="../common/contact.php">Contact Us</a>
-            <a href="./login.html">Login</a>
+            <a href="./login.php">Login</a>
             <a href="./signup.php">Sign-up</a>
         </div>
     </div>
@@ -112,9 +112,9 @@
 </section>
 
 
-    <!--Footer-->
+            <!--Footer-->
 <footer class="text-center text-lg-start bg-dark text-muted">
-
+    
     <!-- Section: Links  -->
     <section class="footer">
     <div class="container text-center text-md-start mt-5">
@@ -122,87 +122,88 @@
         <div class="row mt-3">
         <!-- Grid column -->
         <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-            <!-- Content -->
-            <h6 class="text-uppercase fw-bold mb-4">
-                <i class="fas fa-gem me-3"></i>Zero2Hero
-            </h6>
-            <p>
-                Here you can use rows and columns to organize your footer content. Lorem ipsum
-                dolor sit amet, consectetur adipisicing elit.
-            </p>
+        <!-- Content -->
+        <h6 class="text-uppercase fw-bold mb-4">
+            <i class="fas fa-gem me-3"></i>Zero2Hero
+        </h6>
+        <p>
+            Here you can use rows and columns to organize your footer content. Lorem ipsum
+            dolor sit amet, consectetur adipisicing elit.
+        </p>
         </div>
         <!-- Grid column -->
 
         <!-- Grid column -->
         <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-            <!-- Links -->
-            <h6 class="text-uppercase fw-bold mb-4">
-                Products
-            </h6>
-            <p>
-                <a href="#!" class="text-reset">Angular</a>
-            </p>
-            <p>
-                <a href="#!" class="text-reset">React</a>
-            </p>
-            <p>
-                <a href="#!" class="text-reset">Vue</a>
-            </p>
-            <p>
-                <a href="#!" class="text-reset">Laravel</a>
-            </p>
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            SUBJECTS
+          </h6>
+          <p>
+            <a href="../mathsPages/Mathematics.html" class="text-reset">MATHEMATICS</a>
+          </p>
+          <p>
+            <a href="../tech_page/tech_page(MAIN).html" class="text-reset">TECHNOLOGY</a>
+          </p>
+          <p>
+            <a href="./English page/English.html" class="text-reset">ENGLISH</a>
+          </p>
+          <p>
+            <a href="./science page/schome.html" class="text-reset">SCIENCE</a>
+          </p>
         </div>
         <!-- Grid column -->
 
         <!-- Grid column -->
         <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-            <!-- Links -->
-            <h6 class="text-uppercase fw-bold mb-4">
-                Useful links
-            </h6>
-            <p>
-                <a href="#!" class="text-reset">Pricing</a>
-            </p>
-            <p>
-                <a href="#!" class="text-reset">Settings</a>
-            </p>
-            <p>
-                <a href="#!" class="text-reset">Orders</a>
-            </p>
-            <p>
-                <a href="#!" class="text-reset">Help</a>
-            </p>
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            Useful links
+          </h6>
+          <p>
+            <a href="./AboutUs.html" class="text-reset"> about us </a>
+          </p>
+          <p>
+            <a href="./contact.php" class="text-reset">contact us</a>
+          </p>
+          <p>
+            <a href="./login.php" class="text-reset">login</a>
+          </p>
+          <p>
+            <a href="./signup.php" class="text-reset">sign up</a>
+          </p>
         </div>
         <!-- Grid column -->
 
         <!-- Grid column -->
         <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
-            <!-- Links -->
-            <h6 class="text-uppercase fw-bold mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">
             Contact
-            </h6>
-            <p><i class="fas fa-home me-3"></i> New York, NY 10012, US</p>
-            <p>
+          </h6>
+          <p><i class="fas fa-home me-3"></i> New York, NY 10012, US</p>
+          <p>
             <i class="fas fa-envelope me-3"></i>
             zero2hero.edu@outlook.com
-            </p>
-            <p><i class="fas fa-phone me-3"></i> + 01 234 567 88</p>
-            <p><i class="fas fa-print me-3"></i> + 01 234 567 89</p>
-        </div>
-        <!-- Grid column -->
+          </p>
+          <p><i class="fas fa-phone me-3"></i> + 01 234 567 88</p>
+          <p><i class="fas fa-print me-3"></i> + 01 234 567 89</p>
+            </div>
+            <!-- Grid column -->
         </div>
         <!-- Grid row -->
-    </div>
+        </div>
     </section>
     <!-- Section: Links  -->
 
     <!-- Copyright -->
     <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
-    © 2021 Copyright:
-    <a class="text-reset fw-bold" href="##">Zero2Hero.com</a>
+        &copy; 2021 Copyright:
+        <a class="text-reset fw-bold" href="##">Zero2Hero.com</a>
     </div>
     <!-- Copyright -->
-</footer>
+    </footer>
+    
 <script>
 
 //Javascript of responsive navigation menu
